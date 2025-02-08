4527dc09f8caacbef83365dd20df3720419714c3
Arthur Marc <165643815+ArthurMarc99@users.noreply.github.com>
2025-02-08 23:31:25 +0800

    Update AvailableContacts.vue

--- src/components/AvailableContacts.vue
+++ src/components/AvailableContacts.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex justify-between items-center w-[480px] pb-2">
+    <div class="flex justify-between items-center w-[480px] pb-2 pr-2 pl-2">
         <div class="rounded hover:bg-gray-500 pt-1">
             <div class="flex border-5 border-transparent rounded-full">
-                <img src="/assets/images/smiling.png" alt="Me" class="h-20 w-20 rounded-full">
+                <img src="/assets/images/smiling.png" alt="Me" class="h-22 w-22 rounded-full">
                 <a class="-ml-6 mt-12" href="#"><img src="/assets/images/plus.png" alt="Add" class="rounded-full border-3 h-7 w-7"></a>
             </div>
             <div class="flex justify-center">
@@ -11,7 +11,7 @@
         </div>
         <div class="rounded hover:bg-gray-500 pl-2 pr-2 pt-1">
             <div class="bg-gradient-to-r from-[#f9ce34] via-[#ee2a7b] to-[#6228d7] rounded-full p-1">
-                <img src="/assets/images/eren.png" alt="Friend" class="h-20 w-20 rounded-full border-5">
+                <img src="/assets/images/eren.png" alt="Friend" class="h-22 w-22 rounded-full border-5">
             </div>
             <div class="flex justify-center">
                 <span class="text-white text-sm">guy</span>
@@ -19,7 +19,7 @@
         </div>
         <div class="rounded hover:bg-gray-500 pl-2 pr-2 pt-1">
             <div class="bg-gradient-to-r from-[#f9ce34] via-[#ee2a7b] to-[#6228d7] rounded-full p-1">
-                <img src="/assets/images/eren.png" alt="Friend" class="h-20 w-20 rounded-full border-5">
+                <img src="/assets/images/eren.png" alt="Friend" class="h-22 w-22 rounded-full border-5">
             </div>
             <div class="flex justify-center">
                 <span class="text-white text-sm">guy</span>
@@ -27,7 +27,7 @@
         </div>
         <div class="rounded hover:bg-gray-500 pl-2 pr-2 pt-1">
             <div class="bg-gradient-to-r from-[#f9ce34] via-[#ee2a7b] to-[#6228d7] rounded-full p-1">
-                <img src="/assets/images/eren.png" alt="Friend" class="h-20 w-20 rounded-full border-5">
+                <img src="/assets/images/eren.png" alt="Friend" class="h-22 w-22 rounded-full border-5">
             </div>
             <div class="flex justify-center">
                 <span class="text-white text-sm">guy</span>
